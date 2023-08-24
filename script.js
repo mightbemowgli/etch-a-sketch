e2d7f8c7fc34752859ade777ba032d9893621bf6
Michael Schillinger <mithschill@gmail.com>
2023-08-24 18:28:37 -0500

    Initial changeColor function

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
-const createGrid = size => {
-    const container = document.getElementById('grid-container');
+const container = document.getElementById('grid-container');
 
+let mouseDown = false;
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
+
+const changeColor = (e) => {
+    if (e.type === 'mouseover' && mouseDown || e.type === 'click') {
+        e.target.style.backgroundColor = 'black';
+    }
+}
+
+const createGrid = size => {
     for (let i = 0; i < size ** 2; i++) {
         const div = document.createElement('div');
         div.classList.add('grid-item');
+        div.addEventListener('mouseover', changeColor);
+        div.addEventListener('mousedown', changeColor);
+        div.addEventListener('click', changeColor);
         container.appendChild(div);
     }
 
-    container.style.display = 'grid';
     container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 }
 
-
+// create function for grid size input
 createGrid(16);
+
+
